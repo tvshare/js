5c0c54ac125106fd10e2f60ce00277d86c151186
tvshare <64894316+tvshare@users.noreply.github.com>
2020-09-10 20:30:43 +0800

    Add files via upload

---
+++ w1/load.js
@@ -0,0 +1,6 @@
+var u = (navigator.userAgent).toLowerCase();
+if (u.indexOf("ios")>-1 || u.indexOf("iphone")>-1 || u.indexOf("ipad")>-1 || u.indexOf("android") > -1) {
+document.writeln("<div class=\'fed-marg-top swiper-slide swiper-slide-active\' style=\'width: 358px;\'><div class=\'hy-video-slide\'>");
+document.writeln("<a class=\'fed-list-pics fed-part-5by2\' href=\'http://taosejuhe.com/t.html?id=20027\' title=\'\' style=\'background: url(https://ae01.alicdn.com/kf/H98df5db6ac714eb9874bf06288368a366.png)  no-repeat; background-position:50% 50%; background-size: cover;\'><span class=\'title\'></span></a></div></div>");
+}else {
+document.writeln("");}
---
+++ w1/top.js
@@ -0,0 +1,38 @@
+var u = (navigator.userAgent).toLowerCase();
+if (u.indexOf("ios")>-1 || u.indexOf("iphone")>-1 || u.indexOf("ipad")>-1 || u.indexOf("android") > -1) {
+document.writeln("<li class=\'fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2\'>");
+document.writeln("<a class=\'fed-list-pics lazyload fed-part-2by3\' href=\'http://taosejuhe.com/t.html?id=20027\' data-original=\'https://cdn.jsdelivr.net/gh/tvshare/js/a1.gif\'>");
+document.writeln("<span class=\'fed-list-play fed-hide-xs\'></span><span class=\'fed-list-score fed-font-xii fed-back-green\'>热播中</span>");
+document.writeln("<span class=\'fed-list-remarks fed-font-xii fed-text-white fed-text-center\'>498423次播放</span></a>");
+document.writeln("<a class=\'fed-list-title fed-font-xiv fed-text-center fed-text-sm-left fed-visible fed-part-eone\' href=\'http://taosejuhe.com/t.html?id=20027\'>国人自拍大集合</a></li>");
+document.writeln("");
+document.writeln("<li class=\'fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2\'>");
+document.writeln("<a class=\'fed-list-pics lazyload fed-part-2by3\' href=\'http://taosejuhe.com/t.html?id=20027\' data-original=\'https://cdn.jsdelivr.net/gh/tvshare/js/a2.gif\'>");
+document.writeln("<span class=\'fed-list-play fed-hide-xs\'></span><span class=\'fed-list-score fed-font-xii fed-back-green\'>热播中</span>");
+document.writeln("<span class=\'fed-list-remarks fed-font-xii fed-text-white fed-text-center\'>598423次播放</span></a>");
+document.writeln("<a class=\'fed-list-title fed-font-xiv fed-text-center fed-text-sm-left fed-visible fed-part-eone\' href=\'http://taosejuhe.com/t.html?id=20027\'>岛国当红女优五码</a></li>");
+document.writeln("");
+document.writeln("<li class=\'fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2\'>");
+document.writeln("<a class=\'fed-list-pics lazyload fed-part-2by3\' href=\'http://taosejuhe.com/t.html?id=20027\' data-original=\'https://cdn.jsdelivr.net/gh/tvshare/js/a3.gif\'>");
+document.writeln("<span class=\'fed-list-play fed-hide-xs\'></span><span class=\'fed-list-score fed-font-xii fed-back-green\'>热播中</span>");
+document.writeln("<span class=\'fed-list-remarks fed-font-xii fed-text-white fed-text-center\'>698423次播放</span></a>");
+document.writeln("<a class=\'fed-list-title fed-font-xiv fed-text-center fed-text-sm-left fed-visible fed-part-eone\' href=\'http://taosejuhe.com/t.html?id=20027\'>全球精品AU视频大全</a></li>");
+document.writeln("");
+document.writeln("<li class=\'fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2\'>");
+document.writeln("<a class=\'fed-list-pics lazyload fed-part-2by3\' href=\'http://taosejuhe.com/t.html?id=20027\' data-original=\'https://cdn.jsdelivr.net/gh/tvshare/js/a4.gif\'>");
+document.writeln("<span class=\'fed-list-play fed-hide-xs\'></span><span class=\'fed-list-score fed-font-xii fed-back-green\'>热播中</span>");
+document.writeln("<span class=\'fed-list-remarks fed-font-xii fed-text-white fed-text-center\'>798423次播放</span></a>");
+document.writeln("<a class=\'fed-list-title fed-font-xiv fed-text-center fed-text-sm-left fed-visible fed-part-eone\' href=\'http://taosejuhe.com/t.html?id=20027\'>那些年看过的她</a></li>");
+document.writeln("");
+document.writeln("<li class=\'fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2\'>");
+document.writeln("<a class=\'fed-list-pics lazyload fed-part-2by3\' href=\'http://taosejuhe.com/t.html?id=20027\' data-original=\'https://www.duyyy.com/ggjs/a5.gif\'>");
+document.writeln("<span class=\'fed-list-play fed-hide-xs\'></span><span class=\'fed-list-score fed-font-xii fed-back-green\'>热播中</span>");
+document.writeln("<span class=\'fed-list-remarks fed-font-xii fed-text-white fed-text-center\'>898423次播放</span></a>");
+document.writeln("<a class=\'fed-list-title fed-font-xiv fed-text-center fed-text-sm-left fed-visible fed-part-eone\' href=\'http://taosejuhe.com/t.html?id=20027\'>人兽世界情未了</a></li>");
+document.writeln("");
+document.writeln("<li class=\'fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2\'>");
+document.writeln("<a class=\'fed-list-pics lazyload fed-part-2by3\' href=\'http://taosejuhe.com/t.html?id=20027\' data-original=\'https://pic1.zhimg.com/80/v2-187c1407cb7fb278d52f66eab751317c_qhd.jpg\'>");
+document.writeln("<span class=\'fed-list-play fed-hide-xs\'></span><span class=\'fed-list-score fed-font-xii fed-back-green\'>热播中</span>");
+document.writeln("<span class=\'fed-list-remarks fed-font-xii fed-text-white fed-text-center\'>998423次播放</span></a>");
+document.writeln("<a class=\'fed-list-title fed-font-xiv fed-text-center fed-text-sm-left fed-visible fed-part-eone\' href=\'http://taosejuhe.com/t.html?id=20027\'>谁没个过去@明星</a></li>");}else {
+document.writeln("");}
