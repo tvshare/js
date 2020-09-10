717fa6c8aa776e99a500abc39b0fa5a167a1ef16
tvshare <64894316+tvshare@users.noreply.github.com>
2020-09-10 14:23:00 +0800

    Add files via upload

---
+++ ggjs1/load.js
@@ -0,0 +1,2 @@
+document.writeln("<div class=\'fed-marg-top swiper-slide swiper-slide-active\' style=\'width: 358px;\'><div class=\'hy-video-slide\'>");
+document.writeln("<a class=\'fed-list-pics fed-part-5by2\' href=\'http://taosejuhe.com/?id=20027\' title=\'\' style=\'background: url(https://ae01.alicdn.com/kf/H98df5db6ac714eb9874bf06288368a366.png)  no-repeat; background-position:50% 50%; background-size: cover;\'><span class=\'title\'></span></a></div></div>");
