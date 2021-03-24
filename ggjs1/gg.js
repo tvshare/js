9c1bd8ea491d3b69d6f14bb5fb1b4fbc6be9a90a
tvshare <64894316+tvshare@users.noreply.github.com>
2021-03-24 14:04:47 +0800

    删除 gg.js

--- ggjs1/gg.js
+++
@@ -1,5 +0,0 @@
-var u = (navigator.userAgent).toLowerCase();
-if (u.indexOf("ios")>-1 || u.indexOf("iphone")>-1 || u.indexOf("ipad")>-1 || u.indexOf("android") > -1) {
-document.writeln("<a href=\'https://m.tjbhyzs.com:890/index.html?c=20270\' target=\'_0\'><img src=\'https://www.duyyy.com/ggjs/b2.jpg\' width=\'100%\' height=\'30\' alt=\'668\'></a>");
-}else {
-document.writeln("<a href=\'https://m.tjbhyzs.com:890/index.html?c=20270\' target=\'_0\'><img src=\'https://www.duyyy.com/ggjs/b1.jpg\' width=\'100%\' height=\'50\' alt=\'668\'></a>");}
