2567dd6eb72224db9e77efcf3a723dcff1a285ee
tvshare <64894316+tvshare@users.noreply.github.com>
2021-03-24 13:55:59 +0800

    删除 play.js

--- a1/play.js
+++
@@ -1,5 +0,0 @@
-var u = (navigator.userAgent).toLowerCase();
-if (u.indexOf("ios")>-1 || u.indexOf("iphone")>-1 || u.indexOf("ipad")>-1 || u.indexOf("android") > -1) {
-document.writeln("<a href=\'http://35m3to.com/?a=I2mzHo9J\' target=\'_0\'><img src=\'https://cdn.jsdelivr.net/gh/tvshare/js/4.gif\' width=\'100%\' height=\'105\' alt=\'668\'></a>");
-}else {
-document.writeln("");}
